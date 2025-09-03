df95bb2d493c36ac880fe63f5225bb9b471e6d74
Maria Monchot <mariamonchot@gmail.com>
2025-09-02 21:28:55 -0300

    Uso de Relative y Absolute

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import Button from './components/Button/Button';
-
 function App() {
   return (
-    <div>
-      <h1>Mi Portfolio</h1>
-      <Button text="Ver proyectos" />
-      <Button text="Contacto" variant="secondary" />
-    </div>
-  );
+    <>
+      {/* <Navbar />
+      <About />
+      <Projects />
+      <Contact /> */}
+
+      <div className="body">
+        <div className="about">
+          <div className="about-content">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repellat velit maiores culpa repudiandae et veniam deserunt vel ab assumenda, aspernatur voluptatem expedita. Voluptatibus provident modi minima atque facere laudantium.</p>
+          </div>
+
+          <div className="about-svg">
+            
+          </div>
+
+          
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
