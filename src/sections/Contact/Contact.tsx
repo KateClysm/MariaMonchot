624865ad6b55102d28b36c6663f7ef35bdc0918c
Maria Monchot <mariamonchot@gmail.com>
2025-09-05 23:48:17 -0300

    Json editado: Contact Links aplicados, Estilos De íconos, Hover.

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -5,24 +5,40 @@ import portfolioData from "../../assets/data/portfolio.json";
 const Contact: React.FC = () => {
     return (
       <section className="contact">
-        <img src="src/assets/svgs/waves.svg" alt="Background SVG" className="contact-bg" />
+        <img 
+          src="src/assets/svgs/waves.svg" 
+          alt="Background SVG" 
+          className="contact-bg" 
+        />
+
         <div className="contact-content margin-mark">
           <h2>CONTACT</h2>
           <p>{portfolioData.contact.text}</p>
+
           <div className="contact-icons">
-            {portfolioData.contact.icons.map((iconObj, index) => (
-              <img
-                key={index}
-                src={iconObj.icon}
-                alt={iconObj.alt}
-                className="contact-icon"
-              />
-            ))}
+            {portfolioData.contact.icons.map((iconObj, index) => {
+              
+              let href = iconObj.link;
+
+              return (
+                <a 
+                  key={index} 
+                  href={href} 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={iconObj.icon}
+                    alt={iconObj.alt}
+                    className="contact-icon"
+                  />
+                </a>
+              );
+            })}
           </div>
         </div>
       </section>
     );
-    
 };
 
 export default Contact;
