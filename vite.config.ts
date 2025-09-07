16cb84e4f30cbff69ef21896246d07dbb8cf59ce
Maria Monchot <mariamonchot@gmail.com>
2025-09-07 14:36:59 -0300

    Configuración para el primer deploy de testeo

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:'https://kateclysm.github.io/mariamonchot/'
+  base: 'https://KateClysm.github.io/MariaMonchot/'
 })
